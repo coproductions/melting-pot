1729f331b56c2e5e25a4e85d518a7d7ff3f1084f
coproductions <constantin.dumba@gmail.com>
2023-11-06 05:56:04 -0800

    adds router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import { HashRouter } from "react-router-dom";
 import Signup from "./Signup";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 import "./App.css";
 
+const Thanks = () => <div>Thank you ...</div>;
+
 function App() {
   return (
     <FluentProvider theme={webLightTheme}>
-      <div className="App">
-        <header className='App-header'>
-          <h2>mingle & munch</h2>
-        </header>
-        <main className='content'>
-          <Signup />
-        </main>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <header className="App-header">
+            <h2>mingle & munch</h2>
+          </header>
+          <main className="content">
+            <Routes>
+              <Route path="/" element={<Signup />} />
+              <Route path="/thanks" element={<Thanks />} />
+            </Routes>
+          </main>
+        </div>
+      </HashRouter>
     </FluentProvider>
   );
 }
