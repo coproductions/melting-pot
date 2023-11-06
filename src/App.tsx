ba0e875a09e0536e59285efcbaee29dfab44036d
coproductions <constantin.dumba@gmail.com>
2023-11-05 16:31:41 -0800

    updates styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Signup from "./Signup";
-import {
-  FluentProvider,
-  webLightTheme,
-} from "@fluentui/react-components";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 import "./App.css";
 
 function App() {
   return (
     <FluentProvider theme={webLightTheme}>
       <div className="App">
-        <header>
+        <header className='App-header'>
           <h2>mingle & munch</h2>
         </header>
-        <Signup />
+        <main className='content'>
+          <Signup />
+        </main>
       </div>
     </FluentProvider>
   );
