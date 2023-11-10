5dda18486e35cadc26cc4ef1d2b265e36909c025
coproductions <constantin.dumba@gmail.com>
2023-11-09 16:30:04 -0800

    changes themed button color

--- src/themes/default-theme.ts
+++ src/themes/default-theme.ts
@@ -4,10 +4,14 @@ export const defaultTheme = {
   ...webLightTheme,
   fontFamilyBase: 'Karla',
   defaultFontStyle: { fontFamily: "Karla" },
-  colorBrandBackground: '#EA6696'
+  colorBrandBackground: '#EA6696',
+  colorBrandBackgroundHover: '#dd539b',
+  colorBrandBackgroundPressed: '#fc5ea3',
 };
 
 export const theme2 = {
     ...defaultTheme,
-    colorBrandBackground: '#73ABCD'
+    colorBrandBackground: '#E0723B',
+    colorBrandBackgroundHover: '#4088b3',
+    colorBrandBackgroundPressed: '#81b6cc',
 }
